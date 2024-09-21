b52c89d40b5947f9e9830e65f45932b54646a6e3
Julian Magallanes <100171315+Julian-Magallanes@users.noreply.github.com>
2024-09-20 22:49:03 -0300

    arreglo de cursor

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,10 @@ export default {
 				primary: '#F4881C',
 				borderColor: '#767676',
 			},
+			backgroundImage: {
+				backgroundDefault: 'linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%, #1B1B1B 100%)',
+				backgroundPrimary: "radial-gradient(50% 50% at 50% 50%, #F4881C 0%, rgba(244, 136, 28, 0.00) 100%);"
+			},
 			borderWidth: {
 				small: '0.5px',
 			}
