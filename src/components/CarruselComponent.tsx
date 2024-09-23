1ae4c10a58abf53fb1c4c4e4cc30e47895b357fe
Julian Magallanes <100171315+Julian-Magallanes@users.noreply.github.com>
2024-09-23 16:08:19 -0300

    agregar carrusel

---
+++ src/components/CarruselComponent.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Carousel } from 'antd';
+
+
+
+const CarruselComponent: React.FC = () => {
+  
+
+  return (
+    
+    <Carousel  arrows slidesToShow={3} dots={false}>
+
+      <img src="/julian-profile.webp" alt="" className='w-[300px] rounded-2xl px-2'/>
+      <img src="/marianela-profile.webp" alt="" className='w-[300px] rounded-2xl px-2'/>
+ 
+    </Carousel>
+  );
+};
+
+export default CarruselComponent;
