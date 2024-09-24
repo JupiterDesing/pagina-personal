bd1f36aad66d4d5c63e45fa9c99bba320ecaa0a9
Julian Magallanes <100171315+Julian-Magallanes@users.noreply.github.com>
2024-09-24 12:08:54 -0300

    responsive

--- src/components/CarruselComponent.tsx
+++ src/components/CarruselComponent.tsx
@@ -8,7 +8,7 @@ const CarruselComponent: React.FC = () => {
 
   return (
     
-    <Carousel  arrows slidesToShow={3} dots={false}>
+    <Carousel  arrows slidesToShow={2} dots={false}>
 
       <img src="/julian-profile.webp" alt="" className='w-[300px] rounded-2xl px-2'/>
       <img src="/marianela-profile.webp" alt="" className='w-[300px] rounded-2xl px-2'/>
