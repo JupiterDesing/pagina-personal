87c4423e7341be19d60fa1dffc793c99a3708fa2
Julian Magallanes <100171315+Julian-Magallanes@users.noreply.github.com>
2024-10-04 10:49:48 -0300

    arreglo de jobs y imagen rrss

--- src/components/CarruselComponent.tsx
+++ src/components/CarruselComponent.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Carousel } from 'antd';
 
 const jobs = [
-  {title: 'mockup con logo tierras de mis viejos', image: '/trabajo1.webp'},
-  {title: 'identidad visual de tierras de mis viejos', image: '/trabajo2.webp'},
-  {title: 'mockup marca personal Marisa Perez', image: '/trabajo3.webp'},
+  {title: 'proyecto de landing jupiter', image: '/landing-jupiter.jpeg', href:"https://www.behance.net/gallery/209416989/Landing-Page-UXUI"},
+  {title: 'identidad visual de tierras de mis viejos', image: '/trabajo2.webp',href:"https://www.behance.net/gallery/207395949/IDENTIDAD-DE-MARCA-La-Tierra-de-mis-Viejos"},
+  {title: 'mockup marca personal Marisa Perez', image: '/trabajo3.webp',href:"https://www.behance.net/gallery/207927373/Identidad-de-Marca-Personal-Logo"},
+  {title: 'identidad de marca de naturaleza', image: '/summit-floor.jpeg',href:"https://www.behance.net/gallery/209424405/Brand-Identity-Logo-Nature"},
+  {title: 'mockup de Jupiter Design', image: '/packaging-jupiter.jpeg',href:"https://www.behance.net/gallery/206799851/Brand-identity-Studio-Creativo-Jupiter-Design"},
 ]
 
 const CarruselComponent: React.FC = () => {
@@ -14,7 +16,7 @@ const CarruselComponent: React.FC = () => {
     
     <Carousel arrows slidesToShow={window.innerWidth < 768 ? 1 : 2 } dots={false}>
   {jobs.map((job) => (
-    <div className='w-full rounded-2xl px-2 h-[280px] relative overflow-hidden'>
+    <a className='w-full rounded-2xl px-2 h-[280px] relative overflow-hidden' href={job.href} target="_blank" rel="noopener noreferrer">
       {/* Imagen del job */}
       <img 
         src={job.image} 
@@ -23,7 +25,7 @@ const CarruselComponent: React.FC = () => {
       />
       {/* Máscara */}
       <span className="absolute h-[280px] top-0 left-0 bg-backgroundDefault opacity-100 z-20 w-full  rounded-2xl" />
-    </div>
+    </a>
   ))}
 </Carousel>
   );
